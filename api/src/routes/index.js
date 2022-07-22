da385787bd07110db56a2c96c5ada6f24990ad1b
MarcoAlayn <marc_rob92@hotmail.com>
2022-07-22 00:41:05 -0700

    primer commit

---
+++ api/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.get('/', (req, res) => {
+    res.send("soy la ruta de prueba")
+})
+
+module.exports = router;
