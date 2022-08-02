c22a94d2938d239ee6cb7376679bf53189b4ed6d
MarcoAlayn <marc_rob92@hotmail.com>
2022-08-01 22:08:21 -0700

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 // import './App.css';
 import { Routes, Route } from 'react-router'
+import Home from './components/Home';
 import LandingPage from './components/LandingPage';
+import DetailPokemon from './components/DetailPokemon';
+import CreatePokemon from './components/CreatePokemon';
+import PageNotFound from './components/PageNotFound';
 
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route exact path={'/nav'} element={<LandingPage />} />
+        <Route exact path={'/'} element={<LandingPage />} />
+        <Route exact path={'/home'} element={<Home />} />
+        <Route exact path={'/detail/:id'} element={<DetailPokemon />} />
+        <Route exact path={'/create'} element={<CreatePokemon />} />
+        <Route path={'*'} element={<PageNotFound />} />
       </Routes>
     </div>
   );
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import "../styles/LandingPage.css"
+import { Link } from 'react-router-dom'
+// import background from "../images/background.jpg"
 //va a terner una imagen de background
 //va a tener boton para ir a la home
 // importar el css
@@ -6,7 +9,18 @@ import React from 'react'
 
 function LandingPage() {
     return (
-        <div>LandingPagePage</div>
+        <div className='full'>
+            <div className='full_inner'>
+                <div className='content'>
+                    <h1 className='titulo'>Pokemon APP</h1>
+                    <Link to='/home'>
+                        <button className='btn'>
+                            <span className='ingresar'>START THE ADVENTURE</span>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
