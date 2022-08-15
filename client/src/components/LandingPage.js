ab845b6a191372fa231c0baaf1db79ab1602cf7d
MarcoAlayn <marc_rob92@hotmail.com>
2022-08-15 09:40:47 -0700

    landing ppage css

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './LandingPage.css'
-
+import pikachu from '../images/pikachu.gif'
+import logo from '../images/logo.png'
+import amigos from '../images/amigos.png'
 
 
 function LandingPage() {
     return (
         <div className='conta'>
             <div className='content'>
-                <h1 className='titulo'>Pokemon APP</h1>
-                <Link to='/home'>
-                    <button className='button'>
-                        <span className='ingresar'>START THE ADVENTURE</span>
-                    </button>
-                </Link>
+                <img className='amigos' src={amigos} alt='img not found' />
+                <div className='center' >
+                    <img className='logo' src={logo} alt='img not found' />
+                    <Link to='/home'>
+                        <button className='button'>
+                            <span className='ingresar'>START THE ADVENTURE</span>
+                        </button>
+                    </Link>
+                </div>
+                <div className='image'>
+                    <img src={pikachu} alt='img not found' />
+                </div>
             </div>
         </div>
     )
