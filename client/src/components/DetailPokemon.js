74910f6ae79b74497e22281b19002883f498d892
MarcoAlayn <marc_rob92@hotmail.com>
2022-08-17 05:50:39 -0700

    detail init css

--- client/src/components/DetailPokemon.js
+++ client/src/components/DetailPokemon.js
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPokemonById, resetDetail } from '../redux/actions'
 import NavBar from './NavBar'
+import './DetailPokemon.css'
 
 function DetailPokemon() {
     const navigate = useNavigate();
@@ -25,18 +26,20 @@ function DetailPokemon() {
     }
 
     return (
-        <div>
+        <div className='fullContainer' >
             <NavBar />
-            <div>
+            <div className='detail-container' >
 
                 {
                     myPokemon.length > 0 ?
-                        <div>
-                            <h2>Name:{myPokemon[0].name}</h2>
-                            <img src={myPokemon[0].image} alt='img not found' />
-                            <h4>Id:{myPokemon[0].id}</h4>
-                            <h3>Types:{myPokemon[0].type}</h3>
-                            <div>
+                        <div className='card' >
+
+                            <h2 className='pokeTag' >Name:<span className='pokeName' >{myPokemon[0].name}</span></h2>
+                            <img className='cardImage' src={myPokemon[0].image} alt='img not found' />
+
+                            <div className='Pokevalues'>
+                                <h4>Id:{myPokemon[0].id}</h4>
+                                <h3>Types:{myPokemon[0].type}</h3>
                                 <span>Life:{myPokemon[0].life}</span>
                                 <span>Attack:{myPokemon[0].attack}</span>
                                 <span>Defense:{myPokemon[0].defense}</span>
